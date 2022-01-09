7a42b796805df441cdb9cbe8727c7ec70d91503f
midCenturyDude7 <mgriffes@gmail.com>
2022-01-09 11:16:09 -0800

    Working with RESTful routes and TDD

--- src/api/users.py
+++ src/api/users.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from flask_restx import Resource, Api
+from flask_restx import Resource, Api, fields
 
 from src import db
 from src.api.models import User
@@ -9,19 +9,32 @@ users_blueprint = Blueprint('users', __name__)
 api = Api(users_blueprint)
 
 
+user = api.model('User', {
+    'id': fields.Integer(readOnly=True),
+    'username': fields.String(required=True),
+    'email': fields.String(required=True),
+    'created_date': fields.DateTime,
+})
+
+
 class UsersList(Resource):
 
+    @api.expect(user, validate=True)
     def post(self):
         post_data = request.get_json()
         username = post_data.get('username')
         email = post_data.get('email')
+        response_object = {}
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            response_object['message'] = 'Sorry. That email already exists.'
+            return response_object, 400
 
         db.session.add(User(username=username, email=email))
         db.session.commit()
 
-        response_object = {
-            'message': f'{email} was added!'
-        }
+        response_object['message'] = f'{email} was added!'
         return response_object, 201
 
 
