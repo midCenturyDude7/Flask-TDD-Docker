0d38d74ada6804a96541ee32bc5c84bdba2b58fa
midCenturyDude7 <mgriffes@gmail.com>
2022-01-15 19:56:40 -0800

    Pytest monkeypatching

--- src/api/users.py
+++ src/api/users.py
@@ -4,6 +4,15 @@ from flask_restx import Api, Resource, fields
 from src import db
 from src.api.models import User
 
+from src.api.crud import (  #isort: skip
+    get_all_users,
+    get_user_by_email,
+    add_user,
+    get_user_by_id,
+    update_user,
+    delete_user,
+)
+
 users_blueprint = Blueprint("users", __name__)
 api = Api(users_blueprint)
 
@@ -19,10 +28,33 @@ user = api.model(
 )
 
 
+class UsersList(Resource):
+    @api.marshal_with(user, as_list=True)
+    def get(self):
+        return get_all_users(), 200
+
+    @api.expect(user, validate=True)
+    def post(self):
+        post_data = request.get_json()
+        username = post_data.get("username")
+        email = post_data.get("email")
+        response_object = {}
+
+        user = get_user_by_email(email)
+        if user:
+            response_object["message"] = "Sorry. That email already exists."
+            return response_object, 400
+        
+        add_user(username, email)
+
+        response_object["message"] = f"{email} was added!"
+        return response_object, 201
+
+
 class Users(Resource):
     @api.marshal_with(user)
     def get(self, user_id):
-        user = User.query.filter_by(id=user_id).first()
+        user = get_user_by_id(user_id)
         if not user:
             api.abort(404, f"User {user_id} does not exist")
         return user, 200
@@ -34,57 +66,30 @@ class Users(Resource):
         email = post_data.get("email")
         response_object = {}
 
-        user = User.query.filter_by(id=user_id).first()
+        user = get_user_by_id(user_id)
         if not user:
             api.abort(404, f"User {user_id} does not exist")
 
         if User.query.filter_by(email=email).first():
             response_object["message"] = "Sorry. That email already exists."
             return response_object, 400
-
-        user.username = username
-        user.email = email
-        db.session.commit()
+        
+        update_user(user, username, email)        
 
         response_object["message"] = f"{user_id} was updated!"
         return response_object, 200
 
     def delete(self, user_id):
         response_object = {}
-        user = User.query.filter_by(id=user_id).first()
+        user = get_user_by_id(user_id)
 
         if not user:
             api.abort(404, f"User {user_id} does not exist")
 
-        db.session.delete(user)
-        db.session.commit()
+        delete_user(user)
 
         response_object["message"] = f"{user.email} was removed!"
         return response_object, 200
-
-
-class UsersList(Resource):
-    @api.marshal_with(user, as_list=True)
-    def get(self):
-        return User.query.all(), 200
-
-    @api.expect(user, validate=True)
-    def post(self):
-        post_data = request.get_json()
-        username = post_data.get("username")
-        email = post_data.get("email")
-        response_object = {}
-
-        user = User.query.filter_by(email=email).first()
-        if user:
-            response_object["message"] = "Sorry. That email already exists."
-            return response_object, 400
-
-        db.session.add(User(username=username, email=email))
-        db.session.commit()
-
-        response_object["message"] = f"{email} was added!"
-        return response_object, 201
 
 
 api.add_resource(UsersList, "/users")
